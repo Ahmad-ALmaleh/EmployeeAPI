95814b24fc6bff6a6e3a2c64e591db92d9b53449
Ahmad <ahmadalmaleh99@gmail.com>
2023-11-05 14:49:25 +0200

    first commit

---
+++ EmployeeApi/Controllers/EmployeeController.cs
@@ -0,0 +1,53 @@
+﻿using EmployeeApi.Models;
+using EmployeeApi.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Employee>> GetAll() => EmployeeService.GetAll();
+        [HttpGet("{Id:int}")]
+        public ActionResult<Employee> GetEmployee(int Id)
+        {
+            var employee = EmployeeService.GetEmployee(Id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            else return employee;
+        }
+        [HttpPost]
+        public ActionResult Create(Employee employee)
+        {
+            EmployeeService.Add(employee);
+            return CreatedAtAction(nameof(Create), employee);
+        }
+         [HttpPut("{Id}")]
+        //[HttpPut]
+        public ActionResult Update(int Id,Employee employee)
+        {
+            if(Id != employee.Id)
+                return BadRequest();
+            var existingEmployee = EmployeeService.GetEmployee(Id);
+            if(existingEmployee is null)
+            {
+                return NotFound();
+            }
+            EmployeeService.Update(employee);
+            return NoContent();
+        }
+        [HttpDelete]
+        public ActionResult Delete(int Id)
+        {
+            var employee = EmployeeService.GetEmployee(Id);
+            if (employee == null) { return NotFound(); }
+            EmployeeService.Delete(Id);
+            return NoContent();
+        }
+    }
+}
---
+++ EmployeeApi/Services/EmployeeService.cs
@@ -0,0 +1,43 @@
+﻿using EmployeeApi.Models;
+
+namespace EmployeeApi.Services
+{
+    public class EmployeeService
+    {
+        static List<Employee> employeesList { get; }
+        static int nextEmpId = 3;
+
+        static EmployeeService()
+        {
+            employeesList = new List<Employee>()
+            {
+               // new Employee{Id=0,Name="",Salary=0,Title=""},
+                new Employee{Id=1,Name="Ahmad",Salary=15000,Title="Software Engineer"},
+                new Employee{Id=2,Name="Alaa",Salary=10000,Title="AI Engineer"}
+            };
+        }
+
+        public static List<Employee> GetAll() => employeesList;
+        public static Employee GetEmployee(int Id) => employeesList.FirstOrDefault(e => e.Id == Id);
+        public static void Add(Employee employee)
+        {
+            employee.Id= nextEmpId++;
+            employeesList.Add(employee);
+        }
+        public static void Update(Employee employee)
+        {
+            var index = employeesList.FindIndex(e => e.Id==employee.Id);
+            if (index == -1)
+                return;
+            employeesList[index] = employee;
+        }
+        public static void Delete(int Id)
+        {
+            var employee = GetEmployee(Id);
+            if (employee == null)
+                return;
+            employeesList.Remove(employee);
+        }
+
+    }
+}
